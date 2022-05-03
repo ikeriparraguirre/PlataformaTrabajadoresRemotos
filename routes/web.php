49a575f2c291552bba01ce5a4203c5ab46023033
ikeriparraguirre <iiparraopo1@educacion.navarra.es>
2022-05-03 08:08:57 +0200

    Añadido una imagen que sera la portada del readme

--- routes/web.php
+++ routes/web.php
@@ -23,21 +23,22 @@ Route::get('/', function () {
 });*/
 
 /* Especificamos las rutas de nuestro sitio web y despues con el middleware especificamos si pueden acceder los logeados o no logeados. */
-Route::get('/', function(){
-    return view("login");
+
+Route::get('/', function () {
+    return view('login');
 })->middleware('guest');
-Route::get('/archivos', function(){
+Route::get('/archivos', function () {
     return view('archivos');
 })->middleware('auth');
-Route::get('/registro', function(){
+Route::get('/registro', function () {
     return view('registro');
 })->middleware('guest');
-Route::get('/subir', function(){
+Route::get('/subir', function () {
     return view('subir');
 })->middleware('auth');
 Route::get('/cerrarSesion', [UsuarioController::class, 'cerrarSesion'])->name('cerrarSesion.cerrarSesion')->middleware('auth');
 
 Route::post('/', [UsuarioController::class, 'crearSesion'])->name('crearSesion');
 Route::post('/registro', [UsuarioController::class, 'registrarUsuario'])->name('registro.registrarUsuario');
-Route::post('/subir',[ArchivosController::class ,'subirArchivo']);
+Route::post('/subir', [ArchivosController::class, 'subirArchivo']);
 Route::post('/archivos', [ArchivosController::class, 'leerArchivos']);
